53dded7793b44485ee363e5c18b1a782fef11729
燃烧的冰 <81340116@qq.com>
2020-01-08 11:59:59 +0800

    update admin/index.php.

--- admin/index.php
+++ admin/index.php
@@ -4,13 +4,11 @@ define('MESSAGE_HTM_PATH', ADMIN_PATH . 'view/htm/message.htm');
 define('SKIP_ROUTE', TRUE);
 //$_SERVER['REQUEST_URI'] = str_replace('/admin', '', $_SERVER['REQUEST_URI']);
 // hook admin_index_start.php
-$_SERVER['admin_access'] = TRUE;
-
+$admin_access = TRUE;
 include '../index.php';
 
 // hook admin_index_before.php
-// 后台访问上传文件路径
-$upload_path = $conf['url_rewrite_on'] < 2 ? '../' : '';
+
 $lang += include _include(APP_PATH . "lang/$conf[lang]/lang_admin.php");
 $_SERVER['lang'] = $lang;
 
